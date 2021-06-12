2bdba1b9bc13891e6fb5871e282b506bea59d505
Emily N Mayeski <enmayeski@gmail.com>
2021-06-11 23:14:42 -0400

    prompts enterened and prompt function created to build passwor criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
 // Assignment code here
+var alphaNumericCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`"]
+console.log(alphaNumericCharacters);
 
+var pwdCriteria = function() {
+  var pwdCriteriaLength = prompt("How long would you like the password? The password can be between 8 and 128 characters long.");
+  pwdCriteriaLength = parseInt(pwdCriteriaLength);
+  console.log(pwdCriteriaLength);
+  console.log(typeof pwdCriteriaLength);
+
+  if (pwdCriteriaLength < 8 || pwdCriteriaLength > 128 || pwdCriteriaLength == null) {
+    console.log("Invalid");
+    alert("You must choose a valid number.");
+    return pwdCriteria();
+    } else {
+      console.log("Valid")
+      var pwdCriteriaLower = confirm("Would you like lowercase alphanumeric characters included?");
+      var pwdCriteriaUpper = confirm("Would you like UPPERCASE alphanumeric characters included?");
+      var pwdCriteriaNumbers = confirm("Would you like numbers included?");
+      var pwdCriteriaSpecial = confirm("Would you like special characters included?");
+      console.log(pwdCriteriaLength + pwdCriteriaLower + pwdCriteriaNumbers + pwdCriteriaUpper + pwdCriteriaSpecial);
+    }
+}
+
+pwdCriteria();
+
+// var passwordCriteria = function() {
+//   if (pwdCriteriaLength >= 8 || pwdCriteriaLength >= 128 || typeof(pwdCriteriaLength) !== "number") {
+//     return passwordCriteria(); 
+//   } 
+//   else {
+//   }
+// }
+
+// function name(params) {
+  
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
